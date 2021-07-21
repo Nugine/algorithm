25be0f38f78353132c3149bcb07fe7da9bad825a
Nugine <nugine@foxmail.com>
2021-07-21 14:45:58 +0800

    templates: common

---
+++ templates/common.h
@@ -0,0 +1,23 @@
+#include <algorithm>
+#include <cassert>
+#include <cstdio>
+#include <iostream>
+#include <map>
+#include <vector>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+#define asc(i, s, e) for ((i) = (s); (i) <= (e); ++(i))
+#define rng(i, s, e) for ((i) = (s); (i) < (e); ++(i))
+#define desc(i, s, e) for ((i) = (s); (i) >= (e); --(i))
+#define eprintf(...) fprintf(stderr, __VA_ARGS__)
+// #define eprintf(...)
+
+int main() {
+    cin.sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    // static Sol sol;
+    // sol.run();
+    return 0;
+}
