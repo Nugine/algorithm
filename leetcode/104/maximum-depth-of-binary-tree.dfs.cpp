3c98709d9960334f33080ff1777ab16cfbf881ec
Nugine <nugine@foxmail.com>
2021-10-08 21:01:32 +0800

    leetcode: 104

---
+++ leetcode/104/maximum-depth-of-binary-tree.dfs.cpp
@@ -0,0 +1,27 @@
+// https://leetcode-cn.com/problems/maximum-depth-of-binary-tree/
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right)
+        : val(x), left(left), right(right) {}
+};
+
+#include <algorithm>
+using namespace std;
+
+class Solution {
+  public:
+    int maxDepth(TreeNode *root) {
+        if (root) {
+            int l = maxDepth(root->left);
+            int r = maxDepth(root->right);
+            return max(l, r) + 1;
+        } else {
+            return 0;
+        }
+    }
+};
