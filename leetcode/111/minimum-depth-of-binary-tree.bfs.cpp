0af57a89908a655575a52d39630e8ae015a53631
Nugine <nugine@foxmail.com>
2021-10-08 21:09:36 +0800

    leetcode: 111

---
+++ leetcode/111/minimum-depth-of-binary-tree.bfs.cpp
@@ -0,0 +1,48 @@
+// https://leetcode-cn.com/problems/minimum-depth-of-binary-tree/
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right)
+        : val(x), left(left), right(right) {}
+};
+
+#include <algorithm>
+#include <queue>
+using namespace std;
+
+class Solution {
+  public:
+    int minDepth(TreeNode *root) {
+        if (root == nullptr) {
+            return 0;
+        }
+        queue<TreeNode *> q;
+        q.push(root);
+
+        int ans = 0;
+        while (!q.empty()) {
+            int cnt = q.size();
+            while (cnt--) {
+                auto p = q.front();
+                q.pop();
+
+                if (p->left == nullptr && p->right == nullptr) {
+                    return ans + 1;
+                }
+
+                if (p->left) {
+                    q.push(p->left);
+                }
+                if (p->right) {
+                    q.push(p->right);
+                }
+            }
+            ans += 1;
+        }
+        return ans;
+    }
+};
