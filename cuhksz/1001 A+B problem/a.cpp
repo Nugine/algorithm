19834cfd8dc5271ffcae19ed6e3750077e8d7dbf
Nugine <nugine@foxmail.com>
2023-09-05 16:21:34 +0800

    cuhksz: 1001 A+B problem

---
+++ cuhksz/1001 A+B problem/a.cpp
@@ -0,0 +1,15 @@
+#include <cstdio>
+#include <iostream>
+using namespace std;
+typedef long long ll;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    ll a, b;
+    cin >> a >> b;
+    cout << (a + b) << endl;
+
+    return 0;
+}
